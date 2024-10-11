1e6d026aad1d44e96c58e347b89d1f992030e371
Max <godecmax@gmail.com>
2024-10-11 17:58:03 +0100

    added link to fwp api

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import requests
 import plotly.express as px
 
-#Make an api call and check the response
+import http.client
 
-url = https://football-web-pages1.p.rapidapi.com/match.json
-url += 
+conn = http.client.HTTPSConnection("football-web-pages1.p.rapidapi.com")
+
+headers = {
+    'x-rapidapi-key': "45dfc4c064msh14441388a82d595p138ec2jsn29e66be3c434",
+    'x-rapidapi-host': "football-web-pages1.p.rapidapi.com"
+}
+
+conn.request("GET", "/appearances.json?comp=1&team=1", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
