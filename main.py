b7de9152eace0076907389c3a12132775d5b6157
Max <godecmax@gmail.com>
2024-10-18 16:04:17 +0100

    Cleaned up code, moved functions to separate file

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-import requests
-import plotly.express as px
+from functions import Functions as func
 
-import http.client
-
-conn = http.client.HTTPSConnection("football-web-pages1.p.rapidapi.com")
-
-headers = {
-    'x-rapidapi-key': "45dfc4c064msh14441388a82d595p138ec2jsn29e66be3c434",
-    'x-rapidapi-host': "football-web-pages1.p.rapidapi.com"
-}
-
-conn.request("GET", "/appearances.json?comp=1&team=1", headers=headers)
-
-res = conn.getresponse()
-data = res.read()
-
-print(data.decode("utf-8"))
+#Main body of code
+func.team_picker()
